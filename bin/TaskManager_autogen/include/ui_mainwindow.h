6525620cef2c368fb9df3320f18295ea38164a50
Eduard Zvonarev <edikzvonarev@gmail.com>
2024-03-11 23:34:15 +0300

    UI First

--- bin/TaskManager_autogen/include/ui_mainwindow.h
+++ bin/TaskManager_autogen/include/ui_mainwindow.h
@@ -10,8 +10,11 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
@@ -22,7 +25,10 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QGroupBox *groupBox;
     QMenuBar *menubar;
+    QMenu *menu;
+    QMenu *menu_2;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -30,16 +36,36 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
         MainWindow->resize(800, 600);
+        MainWindow->setStyleSheet(QString::fromUtf8("QGroupBox {\n"
+"	background-color: rgb(255, 255, 221);\n"
+"	border: 2px solid gray;\n"
+"	border-radius: 10px;\n"
+"	align: center;\n"
+"}"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(0, 0, 171, 551));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
         menubar->setGeometry(QRect(0, 0, 800, 22));
+        menu = new QMenu(menubar);
+        menu->setObjectName("menu");
+        menu_2 = new QMenu(menubar);
+        menu_2->setObjectName("menu_2");
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menu->menuAction());
+        menubar->addAction(menu_2->menuAction());
+        menu->addSeparator();
+        menu->addSeparator();
+        menu->addSeparator();
+        menu_2->addSeparator();
 
         retranslateUi(MainWindow);
 
@@ -49,6 +75,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "GroupBox", nullptr));
+        menu->setTitle(QCoreApplication::translate("MainWindow", "\342\211\241", nullptr));
+        menu_2->setTitle(QCoreApplication::translate("MainWindow", "\320\241\320\276\320\267\320\264\320\260\321\202\321\214 \320\267\320\260\320\264\320\260\321\207\321\203", nullptr));
     } // retranslateUi
 
 };
